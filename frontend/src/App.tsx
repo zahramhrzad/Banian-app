335ab0c27e88141042036b7c687d01b19e3e6e7a
zahramhrzad <zhramhrzad@gmail.com>
2026-07-25 09:30:47 +0330

    Setup React + TypeScript + Tailwind project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind کار می‌کند ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
